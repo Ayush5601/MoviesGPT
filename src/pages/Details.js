91b0c94eba4c74ff2a284a805ff4451f51feba1a
Ayush5601 <65501940+Ayush5601@users.noreply.github.com>
2023-11-20 11:38:45 +0530

    Made details page reponsive

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -23,29 +23,29 @@ const Details = () => {
   if (!movieDetails) return null;
 
   return (
-    <div className="text-white bg-gradient-to-bl from-black w-screen h-screen">
+    <div className="w-screen h-screen text-white bg-gradient-to-bl from-black">
       <img
         className="fixed -z-10 object-cover w-screen h-screen"
         src={BACKDROP_CDN_URL + movieDetails.backdrop_path}
         alt="backdrop"
       />
-      <div className="grid grid-flow-col">
-        <div className="col-span-4 mt-36 ml-28">
+      <div className="flex items-center justify-center">
+        <div className="text-2xl md:text-5xl pb-4 -ml-5 font-semibold mr-2 mt-14">
+          {movieDetails.title} - {movieDetails.tagline}
+        </div>
+        {/* <div className="md:text-xl italic ml-2 align-middle">
+          
+        </div> */}
+      </div>
+      <div className="flex flex-col md:grid grid-flow-col">
+        <div className="md:col-span-4 mt-16 ml-28">
           <img
             className="max-h-96 col-span-4"
             alt="Movie Card"
             src={IMG_CDN_URL + movieDetails.poster_path}
           />
         </div>
-        <div className="col-span-8 mt-20 mr-20 ml-6">
-          <div className="flex">
-            <div className="text-5xl pb-4 -ml-5 font-semibold mr-2">
-              {movieDetails.title} -
-            </div>
-            <div className="text-xl self-center italic ml-2">
-              {movieDetails.tagline}
-            </div>
-          </div>
+        <div className="col-span-8 mt-14 mr-20 ml-6">
           <div className="flex pb-8">
             {movieDetails.genres?.map((genre) => (
               <div key={genre.id} className="text-xl">
